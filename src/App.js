430ed5bcf4a8b4afe85d0508dcd5560f50c77520
Piyush Mehta <me@piyushmehta.com>
2018-07-23 17:37:54 +0530

    [WIP]fixures
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.scss";
-import Login from "./Components/Login";
+import Login from "./components/Login";
 class App extends Component {
   render() {
     return (
