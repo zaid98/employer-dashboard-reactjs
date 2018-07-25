0c1955cb70e53c684672bcca7b03ec3793951a8d
zaid98 <hossainzaid98@gmail.com>
2018-07-25 17:46:13 +0530

    sidebar added
    Signed-off-by: zaid98 <hossainzaid98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React, { Component } from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from "./Components/Login";
 import Otp from "./Components/Otp";
-import Tasks from "./Components/Tasks";
-import Landing from "./Components/Landing";
-
-class App extends Component{
-  render(){
-    return(
-      <Router>
-      <div className="App">
-        {/* <ul>
-          <li><Link to={'/'}>Landing</Link></li>
-          <li><Link to={'/Login'}>Login</Link></li>
-          <li><Link to={'/Tasks'}>Tasks</Link></li>
-        </ul> */}
-
-        <Switch>
-          <Route exact path='/' component={Landing}/>
-          <Route exact path='/Login' component={Login}/>
-          <Route exact path='/Otp' component={Otp}/>
-          <Route exact path='/Tasks' component={Tasks}/>
-        </Switch>
+import Sidebar from "./Components/Sidebar";
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="hi">
+        {/* <Login />
+        {/* <Otp /> */}
+        <Sidebar />
       </div>
-    </Router>
     );
   }
 }
